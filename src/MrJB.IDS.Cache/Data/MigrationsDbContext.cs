ea91a84fb6c902654c0d1bb1fd0c856a16f24bd1
mrjamiebowman <mrjamiebowman@gmail.com>
2023-09-26 05:51:29 -0500

    updates to ef migrations

---
+++ src/MrJB.IDS.Cache/Data/MigrationsDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MrJB.IDS.Cache.Data;
+
+public class MigrationsDbContext : ApplicationDbContext
+{
+    public MigrationsDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // override connection string
+        string connectionString = "Persist Security Info=False;User ID=sa;Password=im938eKAW3K7qM0GZ;Initial Catalog=mrjb-identityserver;Server=127.0.0.1;Encrypt=True;TrustServerCertificate=True";
+
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+}
