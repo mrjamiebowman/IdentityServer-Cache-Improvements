c61bcdcd83a15733ac53a133e2e3d9b5992c8f68
mrjamiebowman <mrjamiebowman@gmail.com>
2023-09-26 06:13:23 -0500

    missing ref

--- src/MrJB.IDS.Cache/Migrations/20230926111011_Initial.Designer.cs
+++ src/MrJB.IDS.Cache/Migrations/20230926111011_Initial.Designer.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using MrJB.IDS.Cache.EntityFramework;
 
 #nullable disable
 
